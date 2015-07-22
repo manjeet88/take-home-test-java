7de145c05fd10dd2a64b15116616f274ea906439
Qays H. Poonawala <qayshp@gmail.com>
2015-07-22 01:54:59 -0700

    Convert tabs to spaces. Argh new install of Eclipse.

--- src/main/java/net/qays/maana/TakeHomeTest/CLI.java
+++ src/main/java/net/qays/maana/TakeHomeTest/CLI.java
@@ -14,47 +14,47 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class CLI {
-	static Logger logger = Logger.getAnonymousLogger(CLI.class.getCanonicalName());
+    static Logger logger = Logger.getAnonymousLogger(CLI.class.getCanonicalName());
 
-	public static void main(String[] args) {
-		Options options = new Options();
+    public static void main(String[] args) {
+        Options options = new Options();
 
-		options.addOption(Option.builder("l").longOpt("loglevel").desc("Level of verbosity in logs.").hasArg()
-				.argName("level").numberOfArgs(1).type(Integer.class).build());
-		options.addOption(Option.builder("h").longOpt("help").desc("print this message").build());
-		options.addOption(Option.builder("f").longOpt("followlinks").desc("Follow sym links").hasArg()
-				.argName("followlinks").numberOfArgs(1).type(Boolean.class).build());
-		options.addOption(Option.builder("p").longOpt("path").desc("Top of the tree you want crawled").hasArg()
-				.argName("path").numberOfArgs(1).required().type(File.class).build());
+        options.addOption(Option.builder("l").longOpt("loglevel").desc("Level of verbosity in logs.").hasArg()
+                .argName("level").numberOfArgs(1).type(Integer.class).build());
+        options.addOption(Option.builder("h").longOpt("help").desc("print this message").build());
+        options.addOption(Option.builder("f").longOpt("followlinks").desc("Follow sym links").hasArg()
+                .argName("followlinks").numberOfArgs(1).type(Boolean.class).build());
+        options.addOption(Option.builder("p").longOpt("path").desc("Top of the tree you want crawled").hasArg()
+                .argName("path").numberOfArgs(1).required().type(File.class).build());
 
-		// create the parser
-		CommandLineParser parser = new DefaultParser();
-		try {
-			// parse the command line arguments
-			CommandLine line = parser.parse(options, args);
-			if (line.hasOption('h')) {
-				HelpFormatter formatter = new HelpFormatter();
-				formatter.printHelp(CLI.class.getCanonicalName(), options);
-				return;
-			}
+        // create the parser
+        CommandLineParser parser = new DefaultParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse(options, args);
+            if (line.hasOption('h')) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp(CLI.class.getCanonicalName(), options);
+                return;
+            }
 
-			File path = (File) line.getParsedOptionValue("path");
-			Optional<Integer> loglevel = Optional.of((Integer) line.getParsedOptionValue("loglevel"));
-			Optional<Boolean> followlinks = Optional.of((Boolean) line.getParsedOptionValue("followlinks"));
+            File path = (File) line.getParsedOptionValue("path");
+            Optional<Integer> loglevel = Optional.of((Integer) line.getParsedOptionValue("loglevel"));
+            Optional<Boolean> followlinks = Optional.of((Boolean) line.getParsedOptionValue("followlinks"));
 
-			processPath(path, loglevel, followlinks);
-		} catch (ParseException exp) {
-			// oops, something went wrong
-			logger.log(Level.SEVERE, "test");
-			System.err.println("Parsing failed.  Reason: " + exp.getMessage());
-			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp(CLI.class.getCanonicalName(), options);
-			return;
-		}
-	}
+            processPath(path, loglevel, followlinks);
+        } catch (ParseException exp) {
+            // oops, something went wrong
+            logger.log(Level.SEVERE, "test");
+            System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(CLI.class.getCanonicalName(), options);
+            return;
+        }
+    }
 
-	private static void processPath(File parsedOptionValue, Optional<Integer> loglevel, Optional<Boolean> followlinks) {
+    private static void processPath(File parsedOptionValue, Optional<Integer> loglevel, Optional<Boolean> followlinks) {
 
-	}
+    }
 
 }
