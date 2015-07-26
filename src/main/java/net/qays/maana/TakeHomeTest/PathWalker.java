b81b3d517de2d4acb1e3078a417acd957dd62140
Qays H. Poonawala <qayshp@gmail.com>
2015-07-26 11:27:10 -0700

    Attempt at making Apache VFS work.

--- src/main/java/net/qays/maana/TakeHomeTest/PathWalker.java
+++ src/main/java/net/qays/maana/TakeHomeTest/PathWalker.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileSystemManager;
+import org.apache.commons.vfs2.FileType;
 import org.apache.commons.vfs2.VFS;
 
 import lombok.Builder;
@@ -14,25 +15,56 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PathWalker {
 
+    private static final String REGEX_ZIP_TGZ_TARGZIP = "(zip|ZIP)";
     private final File path;
     private final Boolean followLinks;
 
     public void walk() {
         log.debug("Walk down: {}, follow links: {}", path, followLinks);
         try {
-            // Locate the Jar file
+            // Locate the file
             FileSystemManager fsManager = VFS.getManager();
-            FileObject jarFile = fsManager.resolveFile(new File(System.getProperty("user.home")), path.getPath());
+            FileObject fileObject = fsManager.resolveFile(path.getPath());
 
-            // List the children of the Jar file
-            FileObject[] children = jarFile.getChildren();
-            System.out.println("Children of " + jarFile.getName().getURI());
-            for (int i = 0; i < children.length; i++) {
-                System.out.println(children[i].getName().getBaseName());
+            // List the children of the file
+            log.trace("Children of {}", fileObject.getName().getURI());
+            for (FileObject child : fileObject.getChildren()) {
+                log.trace("Child: {}", child.getName().getBaseName());
+                if (shouldRecurse(child)) {
+                    PathWalker subPath = PathWalker.builder().path(new File(child.toString())).followLinks(followLinks)
+                            .build();
+                    subPath.walk();
+                }
             }
         } catch (FileSystemException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    private boolean shouldRecurse(FileObject file) throws FileSystemException {
+        if (file.getType() == FileType.FOLDER)
+            return true;
+        if (file.getType() == FileType.FILE) {
+            if (file.getName().getExtension().matches(REGEX_ZIP_TGZ_TARGZIP)) {
+                FileSystemManager fsManager = VFS.getManager();
+                FileObject fileObject = fsManager.resolveFile(file);
+
+                log.debug("");
+                return true;
+//            BufferedReader br = new BufferedReader(new InputStreamReader(file.getContent().getInputStream()));
+//            try {
+//                String strLine;
+//                while ((strLine = br.readLine()) != null) {
+//                    // Print the content on the console
+//                    System.out.println(strLine);
+//                }
+//            } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+            }
+        }
+        return false;
     }
 }
